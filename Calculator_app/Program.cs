e011576e8b66cd3abed16976ff74f9fbba695503
tlaleng <rapolaitt@gmail.com>
2018-05-17 13:40:33 +0200

    First commit

--- Calculator_app/Program.cs
+++ Calculator_app/Program.cs
@@ -9,7 +9,97 @@ namespace Calculator_app
     class Program
     {
         static void Main(string[] args)
+            {
+            Console.WriteLine("Welcome to Calculator.");
+            string input = "";
+            StringCalculator calculator= new StringCalculator();
+            double answer = 0.0;
+            while (true)
+            {
+                Console.Write("Calculate: ");
+                input = Console.ReadLine();
+                if(input.CompareTo("e") >= 0)
+                {
+                    Console.WriteLine("Bye Bye...");
+                    break;
+                }
+                else
+                {
+                    answer = calculator.calculate(input);
+                    Console.Write(" Result: " + answer + "\n------------------------\n");
+                }
+                
+
+            }
+        }
+    }
+    public class StringCalculator
+    {
+
+        public StringCalculator()
         {
+        }
+
+        public double calculate(string expression)
+        {
+            /**
+            * EXPECTED EXPRESSION FORM: 12*2+12/2-2*3.5
+            **/
+            double final_answer = 0.0;
+
+            if (isValid(expression))
+            {
+                //expression cleanup
+                string clean_expression = expression.Replace(" ", "");
+
+                clean_expression = clean_expression.Replace(".", ",");  //convert into correct comma
+                clean_expression = clean_expression.Replace("-", "+-"); //convert into addition terms
+                string[] expression_terms = clean_expression.Split('+');//split into terms
+
+                //calculate terms  
+                foreach (string term in expression_terms)
+                {
+                    double term_answer = 0;
+
+                    if (term.Contains("*"))
+                    {
+                        //multiplication
+                        string[] multiplicationNums = term.Split('*');
+                        term_answer = 1.0;
+                        foreach (string num in multiplicationNums)
+                        {
+                            term_answer *= double.Parse(num);
+                        }
+                    }
+                    else if (term.Contains("/"))
+                    {
+                        //division
+                        string[] divisionNums = term.Split('/');
+                        term_answer = double.Parse(divisionNums[0]);
+
+                        for (int i = 1; i < divisionNums.Length; i++)
+                        {
+                            term_answer /= double.Parse(divisionNums[i]);
+                        }
+                    }
+                    else
+                    {//when there's no * and /
+                        term_answer = double.Parse(term);
+                    }
+
+                    final_answer += term_answer;
+                }
+;
+            }
+
+            return final_answer;
+
+        }
+
+        private bool isValid(String expression)
+        {
+
+            return true;
         }
     }
 }
---
+++ Calculator_appTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Calculator_appTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Calculator_appTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("07f1772e-da15-4fcc-8dfc-8ab74d5e3acf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
